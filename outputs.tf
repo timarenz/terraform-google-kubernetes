3670537a5e89024e9e2bec180e8ebce31eda6394
Tim Arenz <tim@arenz.cc>
2019-10-25 13:06:35 +0200

    Reverting to output certs in PEM format

--- outputs.tf
+++ outputs.tf
@@ -3,15 +3,15 @@ output "endpoint" {
 }
 
 output "client_certificate" {
-  value = google_container_cluster.main.master_auth[0].client_certificate
+  value = base64decode(google_container_cluster.main.master_auth[0].client_certificate)
 }
 
 output "client_key" {
-  value = google_container_cluster.main.master_auth[0].client_key
+  value = base64decode(google_container_cluster.main.master_auth[0].client_key)
 }
 
 output "cluster_ca_certificate" {
-  value = google_container_cluster.main.master_auth[0].cluster_ca_certificate
+  value = base64decode(google_container_cluster.main.master_auth[0].cluster_ca_certificate)
 }
 
 output "kubeconfig" {
