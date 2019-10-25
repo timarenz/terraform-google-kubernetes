294ed9ecfd97a8f2d3db4567235cc9af56aeb5d0
Tim Arenz <tim@arenz.cc>
2019-10-25 16:36:08 +0200

    Fixing TF syntax for 0.12 compatibilty, disabling client certs completely

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ data "google_container_engine_versions" "main" {
 }
 
 resource "google_container_cluster" "main" {
-  name               = "${var.name}"
+  name               = var.name
   location           = var.region
   project            = var.project
   network            = var.network
@@ -20,11 +20,6 @@ resource "google_container_cluster" "main" {
   min_master_version = var.kubernetes_version == null ? data.google_container_engine_versions.main.latest_master_version : var.kubernetes_version
   node_version       = var.kubernetes_version == null ? data.google_container_engine_versions.main.latest_master_version : var.kubernetes_version
 
-  # We can't create a cluster with no node pool defined, but we want to only use
-  # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
-  # remove_default_node_pool = true
-  # initial_node_count       = 1
   initial_node_count = var.node_count
 
   node_locations = var.node_locations
@@ -48,7 +43,7 @@ resource "google_container_cluster" "main" {
     password = ""
 
     client_certificate_config {
-      issue_client_certificate = true
+      issue_client_certificate = false
     }
   }
 
@@ -63,26 +58,3 @@ resource "google_container_cluster" "main" {
   }
 
 }
-
-# resource "google_container_node_pool" "main" {
-#   name       = "${var.name}-node-pool"
-#   location   = var.region
-#   project    = var.project
-#   cluster    = "${google_container_cluster.main.name}"
-#   node_count = var.node_count
-#   version    = var.kubernetes_version == null ? data.google_container_engine_versions.main.latest_master_version : var.kubernetes_version
-
-#   node_config {
-#     preemptible     = false
-#     machine_type    = var.machine_size
-#     service_account = var.service_account
-
-#     metadata = {
-#       disable-legacy-endpoints = "true"
-#     }
-
-#     oauth_scopes = var.oauth_scopes
-
-#     labels = local.all_tags
-#   }
-# }
